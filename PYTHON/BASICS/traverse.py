0099cee17816ad9032340644439a9dd4cf64d00d
Vanshika <vanshikag0111@gmail.com>
2021-04-09 17:09:25 +0530

    formatted wrt languages

---
+++ PYTHON/BASICS/traverse.py
@@ -0,0 +1,11 @@
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+#for item in days:      #using for loop
+#   print(item)
+
+
+#using while loop
+i=0
+while (i<len(days)):
+     print(days[i])
+     i=i+1
